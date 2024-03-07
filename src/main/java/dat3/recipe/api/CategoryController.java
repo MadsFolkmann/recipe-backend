f2d23bb5f3e02bda52ee4fc3cf11e73940125bbb
MadsFolkmann <mads1238@gmail.com>
2024-03-07 22:22:01 +0100

    1 commit

---
+++ src/main/java/dat3/recipe/api/CategoryController.java
@@ -0,0 +1,29 @@
+package dat3.recipe.api;
+
+import dat3.recipe.entity.Category;
+import dat3.recipe.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<String> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping()
+    public Category addCategory(@RequestBody Category category) {
+        return categoryService.addCategory(category);
+    }
+}
+
---
+++ src/main/java/dat3/recipe/service/CategoryService.java
@@ -0,0 +1,28 @@
+package dat3.recipe.service;
+
+import dat3.recipe.entity.Category;
+import dat3.recipe.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<String> getAllCategories() {
+        List<Category> categories =  categoryRepository.findAll();
+        //Convert from list of Categories to DTO-type, list of Strings
+        return categories.stream().map((c)->new String(c.getName())).toList();
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+}
+
